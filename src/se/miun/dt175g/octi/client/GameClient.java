cdd80464b2bc4ee11e21694825af11636926df0c
peter <peter.bergh@miun.se>
2023-10-17 19:53:12 +0200

    updated core

--- src/se/miun/dt175g/octi/client/GameClient.java
+++ src/se/miun/dt175g/octi/client/GameClient.java
@@ -2,7 +2,10 @@ package se.miun.dt175g.octi.client;
 
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import se.miun.dt175g.octi.core.Agent;
 import se.miun.dt175g.octi.core.OctiAction;
 import se.miun.dt175g.octi.core.OctiJsonAdapter;
 import se.miun.dt175g.octi.core.OctiState;
@@ -12,13 +15,14 @@ import se.miun.dt175g.octi.core.communicator.PlayerSetup;
 
 // You shouldn't change this class
 public class GameClient {
+	private static final Logger logger = Logger.getLogger(GameClient.class.getName());
 	private Socket clientSocket;
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
-	private StudentAgent studentAgent;
+	private Agent studentAgent;
 	private PlayerSetup setup;
 
-	public GameClient(PlayerSetup setup, StudentAgent studentAgent) {
+	public GameClient(PlayerSetup setup, Agent studentAgent) {
 		this.setup = setup;
 		this.studentAgent = studentAgent;
 	}
@@ -26,16 +30,19 @@ public class GameClient {
 	public void startConnection() {
 		try {
 			clientSocket = new Socket(setup.serverIpAddress, setup.gamePort);
+			//clientSocket.setSoTimeout(5000);  // Set read timeout to 5000 milliseconds
 			out = new ObjectOutputStream(clientSocket.getOutputStream());
 			in = new ObjectInputStream(clientSocket.getInputStream());
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.log(Level.SEVERE, "Error during connection initialization", e);
 		}
 	}
 
 	public void sendPlayerSetup(PlayerSetup setup) {
 		try {
 			out.writeObject(setup);
+		} catch (EOFException e) {
+			System.out.println("Server disconnected unexpectedly. Exiting.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -45,6 +52,9 @@ public class GameClient {
 		try {
 			String jsonState = (String) in.readObject();
 			return jsonState;
+		} catch (EOFException e) {
+			System.out.println("Server disconnected unexpectedly. Exiting.");
+			return null;
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -56,8 +66,9 @@ public class GameClient {
 			Player player = (Player) in.readObject();
 			Player oppPlayer = (Player) in.readObject();
 			long timeLimit = (Long) in.readObject();
-
 			studentAgent.setPlayersAndTimeLimit(player, oppPlayer, timeLimit);
+		} catch (EOFException e) {
+			System.out.println("Server disconnected unexpectedly. Exiting.");
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -66,6 +77,8 @@ public class GameClient {
 	public void sendJsonAction(String action) {
 		try {
 			out.writeObject(action);
+		} catch (EOFException e) {
+			System.out.println("Server disconnected unexpectedly. Exiting.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -86,14 +99,22 @@ public class GameClient {
 		this.sendPlayerSetup(setup);
 		this.receiveAndSetPlayerInfo();
 		while (true) {
-			String stateAsJson = this.receiveStateAsString();
-			OctiState state = OctiState.createStateFromJson(stateAsJson);
-
-			OctiAction action = studentAgent.getNextMove(state);
-
-			String actionJson = OctiJsonAdapter.octiActionToJson(action);
-			this.sendJsonAction(actionJson);
+			try {
+				String stateAsJson = this.receiveStateAsString();
+				if (stateAsJson == null) {
+					logger.log(Level.INFO, "Received null state. Exiting loop.");
+					break;
+				}
+				OctiState state = OctiState.createStateFromJson(stateAsJson);
+				OctiAction action = studentAgent.getNextMove(state);
+				String actionJson = OctiJsonAdapter.octiActionToJson(action);
+				this.sendJsonAction(actionJson);
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, "An exception occurred during play", e);
+				break;
+			}
 		}
+		this.stopConnection();
 	}
 
 }
