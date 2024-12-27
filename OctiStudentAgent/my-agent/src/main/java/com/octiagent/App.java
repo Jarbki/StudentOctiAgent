a88b5fbbbdb3fbc326d85f9de4f7aa331b854cf4
Bjarki Fróðason í Eyðansstovu <2022.187@setur.fo>
2024-12-27 02:15:24 +0000

    maven project setup

---
+++ OctiStudentAgent/my-agent/src/main/java/com/octiagent/App.java
@@ -0,0 +1,13 @@
+package com.octiagent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
