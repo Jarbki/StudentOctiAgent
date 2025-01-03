29d53a736485ffb3547aa343a9eb5dd1a1515c2e
Jarbki <bjarki1208@gmail.com>
2025-01-03 05:00:15 +0000

    ímorgin gon be hard af

--- OctiStudentAgent/my-agent/src/main/java/com/octiagent/StudentAgent.java
+++ OctiStudentAgent/my-agent/src/main/java/com/octiagent/StudentAgent.java
@@ -1,6 +1,5 @@
 package com.octiagent;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,63 +8,75 @@ import se.miun.dt175g.octi.core.OctiAction;
 import se.miun.dt175g.octi.core.Agent;
 import se.miun.dt175g.octi.core.Node;
 
-
 public class StudentAgent extends Agent {
 
 	private List<Node<OctiState, OctiAction>> solutionPath = new ArrayList<>();
 
 	// has the current best move stored
 	private volatile OctiState currentBestMove;
-	
+	private int depthLimit = 30;
+
 	@Override
 	public OctiAction getNextMove(OctiState state) {
+		int currentDepth = 0;
 
-		if (currentBestMove == null){
-			
+		if (currentBestMove == null) {
+
 		}
+
 		// Example of an agent playing just random moves
 		// REMOVE THIS CODE
 		// Random rand = new Random();
 		// var legalActions = state.getLegalActions();
 		// return legalActions.get(rand.nextInt(legalActions.size()));
-		
-		
+
 		Node<OctiState, OctiAction> root = Node.root(state);
-		var gameTree = Agent.generateChildNodes(root);
-		
-		if (state.isTerminal()){
+		List<Node<OctiState, OctiAction>> gameTree = Agent.generateChildNodes(root);
+
+		for (Node<OctiState, OctiAction> node : gameTree) {
+			OctiState nodeState = node.state;
+			var legalActions = nodeState.getLegalActions();
+		}
+
+		if (state.isTerminal()) {
 
 		}
 		// TODO your implementation
 	}
 
+	private void miniMax(int depth, )
+
+
+
+
 	// method that iterates through the tree
-	private void searchTree(){
+	private void searchTree() {
 
 	}
 
 	// method that determines a heuristic for given state
-	private void determineHeuristic(){
+	private void evaluateAction(OctiAction action) {
 
 	}
 
 	// stores a current solution path
-	private void storePath(){
+	private void storePath() {
 
 	}
 
-
-
-	// plan is to see if a solution path can be found, and return a state on that path.
-	// when the opponent makes a move, and a new state is given to the method, then a thread will search the stored solution path 
+	// plan is to see if a solution path can be found, and return a state on that
+	// path.
+	// when the opponent makes a move, and a new state is given to the method, then
+	// a thread will search the stored solution path
 	// to see if the given state is on that path.
-	// Another thread will continuosly try to see if a better solution path can be found from the current state of the game,
-	// and if one is found, replace the current stored solution path. 
-	// this way the algorithm will be able to quickly return a given legal action, even if it might not be the best one.
+	// Another thread will continuosly try to see if a better solution path can be
+	// found from the current state of the game,
+	// and if one is found, replace the current stored solution path.
+	// this way the algorithm will be able to quickly return a given legal action,
+	// even if it might not be the best one.
 	// but given time, the actions will improve.
 
-
-	//objective 1
+	// objective 1
 	// find solution path
 	// implement searching algorithm
 
@@ -75,8 +86,7 @@ public class StudentAgent extends Agent {
 	// objective 3
 	// implement thread to search for possible better solution paths
 
-	// objective 5 
+	// objective 5
 	// develop heuristic function
-
 
 }
